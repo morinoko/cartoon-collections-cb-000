a6d82e281e9b99ce304f75913700f1cd60fc9d46
Felice Forby <feliceforby@gmail.com>
2018-04-15 23:25:43 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,15 +12,23 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any?{ |call| call.length > 4 }
 end
 
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?("cheddar")
-    return "cheddar"
-  elsif array.include?("gouda")
-    return "gouda"
-  elsif array.include?("camembert")
-    return "camembert"
-  else
-    nil
+# def find_the_cheese(array)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   if array.include?("cheddar")
+#     return "cheddar"
+#   elsif array.include?("gouda")
+#     return "gouda"
+#   elsif array.include?("camembert")
+#     return "camembert"
+#   else
+#     nil
+#   end
+# end
+
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
   end
 end
